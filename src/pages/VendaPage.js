0359358b118a962048ff2bb67b17e70b38d27214
Misael dos Santos <misaelbdossantos10@gmail.com>
2020-03-18 23:28:44 -0300

    funcionalidade add produtos no carrinho

--- src/pages/VendaPage.js
+++ src/pages/VendaPage.js
@@ -10,8 +10,11 @@ import {
   Alert,
 } from 'react-native';
 import FormRow from '../components/FormRow';
-import {connect} from 'react-redux';
-import {setField, salvarProduto} from '../actions';
+import { connect } from 'react-redux';
+import { setField, salvarProduto } from '../actions';
+
+import _ from 'lodash'
+import { TouchableOpacity } from 'react-native';
 
 class CadastroPage extends React.Component {
   constructor(props) {
@@ -19,11 +22,35 @@ class CadastroPage extends React.Component {
 
     this.state = {
       isLoading: false,
+      produtosSearched: [],
+      // produtos a serem adicionados
+      // deveria ser colocado no state do redux (?)
+      produtosCarrinho: []
     };
   }
 
+  // simulação dos produtos
+  // caso usar api, não é necessário armazenar em memória
+  // sugestão: criar algo do tipo: produto = {descricao, id} em vez de: produto = {pro_codigo, prod_descricao}
+  produtos = require("../../produtos.json")
+
+  searchProdutos = _.debounce((term) => {
+    term = term.toLowerCase()
+    // faz busca via js
+    // depois é necessário fazer busca via api
+    this.setState({
+      produtosSearched: this.produtos.filter(produto => produto.pro_descricao.toLowerCase().includes(term))
+    })
+  })
+
+  addProduto = (produto) => {
+    this.setState({
+      produtosCarrinho: [...this.state.produtosCarrinho, produto]
+    })
+  }
+
   render() {
-    const {cadPro, setField} = this.props;
+    const { cadPro, setField } = this.props;
 
     return (
       <ScrollView style={styles.container}>
@@ -35,17 +62,57 @@ class CadastroPage extends React.Component {
               placeholderTextColor="#91a9cf"
               autoCapitalize="words"
               value={cadPro.pro_descricao}
-              onChangeText={value => setField('pro_descricao', value)}
+              onChangeText={this.searchProdutos}
+
             />
+
           </FormRow>
-          <FormRow>
-            <View style={styles.viewButton}>
-              <Button title={'Adicionar'} />
-            </View>
-          </FormRow>
+          {!!this.state.produtosSearched.length && <View
+            style={{
+              backgroundColor: 'white',
+              width: '100%',
+            }}
+          >
+            {this.state.produtosSearched.map((produto) => {
+              // faz a renderização dos produtos filtrados a partir da busca
+              // customizar...
+              return <TouchableOpacity
+                onPress={() => {
+                  // add no carrinho
+                  this.addProduto(produto)
+                }}
+                key={produto.pro_codigo}
+              >
+                <Text
+                  style={{
+                    fontSize: 20,
+                    padding: 10,
+                    textAlign: 'center'
+                  }}
+                >
+                  {produto.pro_descricao}
+                </Text>
+              </TouchableOpacity>
+            })}
+          </View>}
+          {/* <FormRow>
+              <View style={styles.viewButton}>
+                <Button title={'Adicionar'} />
+              </View>
+            </FormRow> */}
           <FormRow>
             <View style={styles.inputForm}>
-              <Text>teste</Text>
+              {/* <Text>teste</Text> */}
+              {this.state.produtosCarrinho.map((produto) => {
+                // renderização dos produtos no carrinho
+                return <Text key={produto.pro_codigo} style={{
+                  color: 'white',
+                  fontSize: 20,
+                  padding: 10,
+                }}>
+                  {produto.pro_descricao}
+                </Text>
+              })}
             </View>
           </FormRow>
         </View>
@@ -53,26 +120,27 @@ class CadastroPage extends React.Component {
           {this.state.isLoading ? (
             <ActivityIndicator size="large" color="#FFF" />
           ) : (
-            <Button
-              title={'Salvar'}
-              color={'green'}
-              onPress={async () => {
-                this.setState({isLoading: true});
-                try {
-                  const {cadPro, salvarProduto} = this.props;
-                  await salvarProduto(cadPro);
-                  //   navigation.goBack(); .volta a página anterior.;
-                } catch (error) {
-                  Alert.alert('Erro!', error.message);
-                } finally {
-                  this.setState({isLoading: false});
-                }
-              }}
-            />
-          )}
+              <Button
+                title={'Salvar'}
+                color={'green'}
+                onPress={async () => {
+                  this.setState({ isLoading: true });
+                  try {
+                    // agora é possível pegar os produtos do carrinho e salvar aqui
+                    const { cadPro, salvarProduto } = this.props;
+                    await salvarProduto(cadPro);
+                    //   navigation.goBack(); .volta a página anterior.;
+                  } catch (error) {
+                    Alert.alert('Erro!', error.message);
+                  } finally {
+                    this.setState({ isLoading: false });
+                  }
+                }}
+              />
+            )}
         </View>
         <View style={styles.marginFooter} />
-      </ScrollView>
+      </ScrollView >
     );
   }
 }
@@ -109,7 +177,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     backgroundColor: '#113063',
   },
-  marginFooter: {marginBottom: 10},
+  marginFooter: { marginBottom: 10 },
 });
 
 function mapStateToProps(state) {
